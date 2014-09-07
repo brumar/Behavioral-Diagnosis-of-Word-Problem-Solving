64426a4acbfc79d3436bd1cbda827f5f75ff2ec8
avannest <vanneste.adelie@gmail.com>
2014-09-07 10:01:14 +0200

    better enum.type_de_resolution

--- enum.type_de_resolution.php
+++ enum.type_de_resolution.php
@@ -3,10 +3,11 @@
 // Enum: type de resolution
 abstract class Type_de_Resolution
 {
-	const	addition_a_trou = 0;
-	const	soustraction_simple = 1;
-	const	soustraction_inversee = 2;
-	const	operation_mentale = 3;
+	const	addition_simple = 0;
+	const	addition_a_trou = 1;
+	const	soustraction_simple = 2;
+	const	soustraction_a_trou = 3;
+	const	operation_mentale = 4;
 
 	private function	_construct(){}
 }
@@ -15,11 +16,17 @@ function	print_tdr($type_de_resolution)
 {
 	switch($type_de_resolution)
 	{
-		case Type_de_Resolution::addition :
-			echo "addition";
+		case Type_de_Resolution::addition_simple :
+			echo "addition simple";
 			break;
-		case Type_de_Resolution::soustraction :
-			echo "soustraction";
+		case Type_de_Resolution::addition_a_trou :
+			echo "addition a trou";
+			break;
+		case Type_de_Resolution::addition_a_trou :
+			echo "addition a trou";
+			break;
+		case Type_de_Resolution::soustraction_a_trou :
+			echo "soustraction a trou";
 			break;
 		default :
 			echo "(type de resolution non reconnu)";
