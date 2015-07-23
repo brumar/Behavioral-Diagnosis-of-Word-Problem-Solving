8d61d3689f013e3d034f2e40c0de65b52a1ad8c1
brumar <martin.bruno.mail@gmail.com>
2015-07-23 08:49:30 +0200

    method to better check the equality of expressions

--- class.comparator.php
+++ class.comparator.php
@@ -8,8 +8,11 @@ class	Comparator
 	public	$s1;
 	public	$s2;
 	public $logger;
+	public $regexParenthesis="/(\((?:[^\(]*?)\))/";
 
 	public function		Comparator($symbols,$s1,$s2,$logger) //TODO: Would better without s1 and s2 as argument for constructor
+	//s1 is condidered to be the human coding this is important because we suppose that
+	//it can has less parenthesis than in the computer coding (e.g T1+p1+d can be found in human coding)
 	{
 		$this->logger=$logger;
 		$this->symbols=$symbols;
@@ -31,18 +34,61 @@ class	Comparator
 		if($this->s1==$this->s2){
 			return True;
 		}
-		if(empty($this->s1)||empty($this->s2)){ // if both are void, the condition $s1==$s2 is valid
+		if(empty($this->s1)||empty($this->s2)){ // if both are void, the previous condition $s1==$s2 is valid
 			return False;
 		}
-		return ($this->haveSameNumberOfSymbols() && $this->compareExpressionValue());
+		
+		
+		return ($this->CheckInsideParenthesis() && $this->haveSameNumberOfSymbols() && $this->compareExpressionValue());
+	}
+	
+	public function CheckInsideParenthesis(){
+		$nestedValues_s1=$this->extractParenthesisValues($this->s1);
+		$nestedValues_s2=$this->extractParenthesisValues($this->s2);
+		foreach(array_keys($nestedValues_s1) as $k){
+			foreach($nestedValues_s1[$k] as $val){
+				$countS2=$k;
+				$found=False;
+				while(array_key_exists($countS2,$nestedValues_s2)){
+					if(in_array($val,$nestedValues_s2[$countS2])){
+						$found=True;
+						break;
+					}
+					$countS2+=1;
+				}
+				if($found==False){
+					return False;
+				}
+			}
+		}
+		return True;
+	}
+	
+	public function extractParenthesisValues($expression){
+		//
+		$val=[];
+		$expression_temp=$expression;
+		$nestedCounter=0;
+		while(preg_match_all($this->regexParenthesis,$expression_temp, $matches)!=0){
+			$nestedCounter+=1;
+			$val[$nestedCounter]=[];
+			$pars=$matches[0];
+			foreach($pars as $par){
+				$this->evaluator->evaluate('f(t1,p1,d)='.$par);
+				$res = $this->evaluator->evaluate('f(16,4,1)');
+				$expression_temp=str_replace($par,$res,$expression_temp);
+				$val[$nestedCounter][]=$res;
+			}
+		}
+		return $val;
 	}
 	
 	public function compareExpressionValue(){
 		try {
-		$this->evaluator->evaluate('f(t1,p1,d)='.$this->s1);
-		$this->evaluator->evaluate('g(t1,p1,d)='.$this->s2);
-		$res1 = $this->evaluator->evaluate('f(16,4,1)');
-		$res2 = $this->evaluator->evaluate('g(16,4,1)');
+			$this->evaluator->evaluate('f(t1,p1,d)='.$this->s1);
+			$this->evaluator->evaluate('g(t1,p1,d)='.$this->s2);
+			$res1 = $this->evaluator->evaluate('f(16,4,1)');
+			$res2 = $this->evaluator->evaluate('g(16,4,1)');
 		} catch (Exception $e) {
 			echo('mathEval failed to compare '.$this->s1.' with '.$this->s2);
 			return False;
